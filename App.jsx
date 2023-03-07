9642fa7ef1270577ceea0b77f03f45b72fe44615
TheBigH50 <HFangmeier@outlook.com>
2023-03-07 16:38:11 -0600

    test

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-400">
+      <Text className="text-gray-800 text-xl">Hello, world!</Text>
+    </View>
+  );
+};
+
+export default HelloWorldApp;
